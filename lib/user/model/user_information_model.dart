b941a71ee66f6847fd35133711a653633c5305e5
Vijay Sheoran <vijaysirsi1997@gmail.com>
2024-01-15 17:17:40 +0530

    model class create

---
+++ lib/user/model/user_information_model.dart
@@ -0,0 +1,58 @@
+import 'package:user_wave/user/model/address_model.dart';
+import 'package:user_wave/user/model/compnay_model.dart';
+
+class UserInformation {
+  int? id;
+  String? name;
+  String? username;
+  String? email;
+  Address? address;
+  String? phone;
+  String? website;
+  Company? company;
+
+  UserInformation(
+      {this.id,
+        this.name,
+        this.username,
+        this.email,
+        this.address,
+        this.phone,
+        this.website,
+        this.company});
+
+  UserInformation.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    name = json['name'];
+    username = json['username'];
+    email = json['email'];
+    address =
+    json['address'] != null ? new Address.fromJson(json['address']) : null;
+    phone = json['phone'];
+    website = json['website'];
+    company =
+    json['company'] != null ? new Company.fromJson(json['company']) : null;
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['id'] = this.id;
+    data['name'] = this.name;
+    data['username'] = this.username;
+    data['email'] = this.email;
+    if (this.address != null) {
+      data['address'] = this.address!.toJson();
+    }
+    data['phone'] = this.phone;
+    data['website'] = this.website;
+    if (this.company != null) {
+      data['company'] = this.company!.toJson();
+    }
+    return data;
+  }
+}
+
+
+
+
+
